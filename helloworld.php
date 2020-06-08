da6016ed537de34577f3356e6563114179923862
root <Sheriffo Ceesay>
2020-06-08 16:05:08 +0100

    lab 0 and initial commit

---
+++ helloworld.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My First PHP Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="jumbotron text-center">
+  <?php echo "<h1>My First PHP Lab</h1>" ?>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+        <?php 
+        echo 
+        "<ul>
+            <li>Mango</li>
+            <li>Orange</li>
+        </ul>" 
+        ?>
+    </div>
+    <div class="col-sm-4">
+      
+    </div>
+    <div class="col-sm-4">
+     
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+  // Defining variables
+  $txt = "Hello World!";
+  $num = 123456789;
+  $colors = array("Red", "Green", "Blue");
+  var_dump($txt); echo "<br>";
+  // Displaying variables
+  echo $txt;
+  print "<br>";
+  print $num;
+  echo "<br>";
+  print $colors[0];
+
+  echo("My name", "is sheriffo");
+?>
+
